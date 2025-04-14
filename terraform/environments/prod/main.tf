b004d9af599aec68b05b826f47b4042ca45f7dd8
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-04-14 14:03:05 -0400

    fix terraform formatting

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -4,8 +4,8 @@ module "aks" {
   location            = module.network.location
   resource_group_name = module.network.resource_group_name
   node_count          = 1
-  
- 
+
+
   default_node_pool {
     name                = "default"
     vm_size             = "Standard_B2s"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "region" {
-  default     = "canadacentral"
+  default = "canadacentral"
 }
